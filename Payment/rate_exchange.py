926dc7394233fe7a14c0ba7405d4a5f572658ef9
shadyelnady <shadyelnady@gmail.com>
2022-11-22 11:24:21 +0200

    add

---
+++ Payment/rate_exchange.py
@@ -0,0 +1,8 @@
+# from djmoney.contrib.exchange.backends import FixerBackend, OpenExchangeRatesBackend
+# from djmoney.contrib.exchange.models import get_rate
+
+
+# OpenExchangeRatesBackend().update_rates()
+# FixerBackend().update_rates()
+# get_rate('USD', 'EUR', backend=OpenExchangeRatesBackend.name)
+# get_rate('USD', 'EUR', backend=FixerBackend.name)

0a9678e198542c1d45800ae312a40f74a7d9e529
shadyelnady <shadyelnady@gmail.com>
2022-11-23 13:15:54 +0200

    add

--- Config/urls.py
+++ Config/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     path("", include("django.contrib.auth.urls")),
     path("", include("Laboratory.urls", namespace="Laboratory")),
     path("", include("GraphQL.urls", namespace= "GraphQL")),
-    path("", include("Digit.urls", namespace="Digit")),
+    path("digit/", include("Digit.urls", namespace="Digit")),
     path("payment", include("Payment.urls", namespace= "Payment")),
     path('admin/', admin.site.urls),
     path("__debug__/", include(debug_toolbar.urls)),
